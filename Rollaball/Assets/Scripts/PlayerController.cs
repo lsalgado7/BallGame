a117d2afadc708fe9e0740f84b81b8a4e8d5ff17
lsalgado7 <levirsalgado@gmail.com>
2025-04-05 15:16:07 -0700

    Initial Commit...
    Built most of the game, hopefully this goes well.

---
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private int count;
+
+    private float movementX;
+    private float movementY;
+
+    public float speed = 0;
+
+    public TextMeshProUGUI countText;
+
+    public GameObject winTextObject;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        winTextObject.SetActive(false);
+        count = 0;
+        SetCountText();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 4)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    /*
+    void Update()
+    {
+        
+    }
+    */
+}
